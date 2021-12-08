e2e4fc0f143d61ede40b6701778e93088ed4ab27
gabrierr <82598985+gabrierr@users.noreply.github.com>
2021-12-08 14:56:58 +0700

    Update mirror.py

--- bot/modules/mirror.py
+++ bot/modules/mirror.py
@@ -394,14 +394,14 @@ def _mirror(bot, update, isZip=False, extract=False, isQbit=False, isLeech=False
             pass
 
     if not bot_utils.is_url(link) and not bot_utils.is_magnet(link) and not os.path.exists(link):
-        help_msg = "<b>Send link along with command line:</b>"
-        help_msg += "\n<code>/command</code> {link} |newname pswd: mypassword [𝚣𝚒𝚙/𝚞𝚗𝚣𝚒𝚙]"
-        help_msg += "\n\n<b>By replying to link or file:</b>"
-        help_msg += "\n<code>/command</code> |newname pswd: mypassword [𝚣𝚒𝚙/𝚞𝚗𝚣𝚒𝚙]"
-        help_msg += "\n\n<b>Direct link authorization:</b>"
-        help_msg += "\n<code>/command</code> {link} |newname pswd: mypassword\nusername\npassword"
-        help_msg += "\n\n<b>Qbittorrent selection:</b>"
-        help_msg += "\n<code>/qbcommand</code> <b>s</b> {link} or by replying to {file}"
+        help_msg = f'<b>Send link along with command line:</b>'
+        help_msg += f'\n<code>/command</code> {link} |newname pswd: mypassword [𝚣𝚒𝚙/𝚞𝚗𝚣𝚒𝚙]''
+        help_msg += f'\n\n<b>By replying to link or file:</b>'
+        help_msg += f'\n<code>/command</code> |newname pswd: mypassword [𝚣𝚒𝚙/𝚞𝚗𝚣𝚒𝚙]'
+        help_msg += f'\n\n<b>Direct link authorization:</b>'
+        help_msg += f'\n<code>/command</code> {link} |newname pswd: mypassword\nusername\npassword'
+        help_msg += f'\n\n<b>Qbittorrent selection:</b>'
+        help_msg += f'\n<code>/qbcommand</code> <b>s</b> {link} or by replying to {file}'
         return sendMessage(help_msg, bot, update)
 
     LOGGER.info(link)
